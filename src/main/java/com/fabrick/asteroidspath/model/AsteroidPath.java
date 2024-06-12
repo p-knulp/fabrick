95afd5e14833f847561dfba93e270938b5a758db
Paolo Mastroianni <paolo.mastroianni@sourcesense.com>
2024-06-12 17:32:27 +0200

    chores -aggiunta la parte lombok e cambiato ascolto server su porta 7070

--- src/main/java/com/fabrick/asteroidspath/model/AsteroidPath.java
+++ src/main/java/com/fabrick/asteroidspath/model/AsteroidPath.java
@@ -1,60 +1,18 @@
 package com.fabrick.asteroidspath.model;
 
-//import lombok.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
-// per il momemento commentata la parte lombok
-/*
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-*/
 
 public class AsteroidPath {
     private LocalDate fromDate;
     private LocalDate toDate;
     private String fromPlanet;
     private String toPlanet;
-
-    public AsteroidPath(LocalDate fromDate, LocalDate toDate, String fromPlanet, String toPlanet) {
-        this.fromDate = fromDate;
-        this.toDate = toDate;
-        this.fromPlanet = fromPlanet;
-        this.toPlanet = toPlanet;
-    }
-
-    public LocalDate getFromDate() {
-        return fromDate;
-    }
-
-    public void setFromDate(LocalDate fromDate) {
-        this.fromDate = fromDate;
-    }
-
-    public LocalDate getToDate() {
-        return toDate;
-    }
-
-    public void setToDate(LocalDate toDate) {
-        this.toDate = toDate;
-    }
-
-    public String getFromPlanet() {
-        return fromPlanet;
-    }
-
-    public void setFromPlanet(String fromPlanet) {
-        this.fromPlanet = fromPlanet;
-    }
-
-    public String getToPlanet() {
-        return toPlanet;
-    }
-
-    public void setToPlanet(String toPlanet) {
-        this.toPlanet = toPlanet;
-    }
 }
